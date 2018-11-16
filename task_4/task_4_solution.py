2a0e53d922745de5e3aca95d6a8a013aecb6a80e
v1leftw <dmitry.g.fake@gmail.com>
2018-11-16 19:32:54 +0300

    fixed task 4

--- task_4/task_4_solution.py
+++ task_4/task_4_solution.py
@@ -1,16 +1,28 @@
 import sys
 import os
 from tinytag import TinyTag
+from itertools import groupby
 
 
 def get_args():
+    """
+        - why are we not using argparse?:)
+        - use it if u can
+        - would like to use it,
+        but in site script calls with argument like that:
+        $python3 sctipt.py "argument"
+        for argparse we need use it like (as far as i know):
+        $python3 script.py -argument "argument"
+        -------------------------
+        artist_name = sys.argv[1]
+    """
     return sys.argv[1]
 
 
 def get_mp3_files(directory_path):
     for root, dirs, filenames in os.walk(directory_path, topdown=True):
         for filename in filenames:
-            if filename.endswith(".mp3"):
+            if filename.endswith('.mp3'):
                 yield os.path.abspath(os.path.join(root, filename))
 
 
@@ -19,20 +31,54 @@ def get_tags(path_to_mp3_file):
     return tags
 
 
-def create_music_collection(directory_path):
+def create_music_collection(mp3_files):
     collection = []
-    for mp3_file_path in get_mp3_files(directory_path):
-        collection.append([mp3_file_path, get_tags(mp3_file_path)])
-    print(type(collection))
+    for mp3_file in mp3_files:
+        collection.append([mp3_file, get_tags(mp3_file)])
     return collection
 
 
-def sort_music_collection_by_artist_name(artist_name, collection):
-    for _path, tags in collection:
-        print(tags.artist)
+def filter_collection_by_criteria(collection, criteria):
+    return [
+        [_path, tags]
+        for _path, tags in collection
+        if is_equal_to_given_artist_name(criteria, tags)
+    ]
+
+
+def is_equal_to_given_artist_name(criteria, tag):
+    return tag.artist == criteria
+
+
+def group_collection_by_album(collection):
+    for group, items in groupby(collection, key=lambda item: item[1].album):
+        yield group, list(items)
+
+
+def pretty_print(_grouped_collection):
+    for album, files in _grouped_collection:
+        print(album)
+        for file_counter, [_path, tags] in enumerate(files, 1):
+            print('{:<4}{}. "{}" {} ({})'.format(
+                ' ',
+                file_counter,
+                tags.title,
+                beatufy_duration(tags.duration),
+                _path))
+
+
+def beatufy_duration(duration):
+    minutes = duration / 60
+    seconds = duration % 60
+    return '{}:{}'.format(str(minutes)[:1], str(seconds)[:2])
 
 
 if __name__ == '__main__':
     artist = get_args()
-    path = "music"
-    sort_music_collection_by_artist_name(artist, create_music_collection(path))
+    path = 'music'
+    mp3_collection = create_music_collection(get_mp3_files(path))
+    if not mp3_collection:
+        exit('No mp3 files found')
+    filtered_collection = filter_collection_by_criteria(mp3_collection, artist)
+    grouped_collection = group_collection_by_album(filtered_collection)
+    pretty_print(grouped_collection)
